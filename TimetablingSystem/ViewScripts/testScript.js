da5617db37d89f020b7cf9dae2abf65f49f88f09
nathan1993 <nathan_n1993@yahoo.co.uk>
2014-05-11 22:40:39 +0100

    Text size

--- TimetablingSystem/ViewScripts/testScript.js
+++ TimetablingSystem/ViewScripts/testScript.js
@@ -4,3 +4,9 @@
 
 });
 
+function changeFontSize(element, step) {
+    step = parseInt(step, 10);
+    var el = document.getElementById(element);
+    var curFont = parseInt(el.style.fontSize, 10);
+    el.style.fontSize = (curFont + step) + 'px';
+}
