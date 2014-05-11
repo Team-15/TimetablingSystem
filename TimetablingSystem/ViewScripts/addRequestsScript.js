e22f8560a3d5abdd3a47291388f87e7246f5c3a3
Daniel Cohen <analoguecolour@gmail.com>
2014-05-11 03:24:13 +0100

    fixed room listing

--- TimetablingSystem/ViewScripts/addRequestsScript.js
+++ TimetablingSystem/ViewScripts/addRequestsScript.js
@@ -100,7 +100,7 @@ function facilityPopulate() {
             tempStr += "</tr><tr>";
         }
     }
-    tempStr += "<tr><td>Number of students: <input type='textbox' id='CAP' onchange='infoStore()' onclick='infoStore()'></td></tr>";
+    tempStr += "<tr><td>Number of students: <input type='textbox' id='CAP' onchange='infoStore()' onclick='infoStore()' value='50'></td></tr>";
     tempStr += "<tr><td>Park: <select id='PRK' onchange='infoStore()' onclick='infoStore()'>";
     for (var j = 0; j < parksArray.length; j++) {
         tempStr += "<option value='"+ parksArray[j] + "'>" + parksArray[j] + "</option>";
