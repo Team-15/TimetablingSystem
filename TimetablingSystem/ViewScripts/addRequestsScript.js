685b2b754ea3b46c8237362558d885436d7c79c0
Daniel Cohen <analoguecolour@gmail.com>
2014-05-11 18:44:11 +0100

    fixed spacing of week row

--- TimetablingSystem/ViewScripts/addRequestsScript.js
+++ TimetablingSystem/ViewScripts/addRequestsScript.js
@@ -31,7 +31,7 @@ function weekCreator() {
     for (var i = 0; i < numberOfWeeks; i++) {
         tempStr += "<input style='display:none' type='checkbox' class='wkInput' id='weekChoice" + i + "' onclick=''><label for='weekChoice" + i + "' class='btn btn-default'>" + (i + 1) + "</label>";
     }
-    tempStr += " | ";
+    tempStr += "&nbsp;|&nbsp;";
     tempStr += "</td><td><input class='btn btn-default' type='button' value='default' onclick='setWeeks(regularWeeks)'>";
     tempStr += "<input class='btn btn-default' type='button' value='all' onclick='setWeeks(numberOfWeeks)'>";
     tempStr += "<input class='btn btn-default' type='button' value='odd' onclick='setWeeks(-1)'>";
