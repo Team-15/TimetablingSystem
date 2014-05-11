37b77d771c86b3bb24ec21910559d275eefe0485
nathan1993 <nathan_n1993@yahoo.co.uk>
2014-05-11 23:37:20 +0100

    TextSize

--- TimetablingSystem/ViewScripts/testScript.js
+++ TimetablingSystem/ViewScripts/testScript.js
@@ -3,10 +3,28 @@
    
 
 });
+function defaultFont(element) {
+    var el = document.getElementById(element);
+    el.style.fontSize = 12 + 'px';
+
+}
 
 function changeFontSize(element, step) {
-    step = parseInt(step, 10);
     var el = document.getElementById(element);
-    var curFont = parseInt(el.style.fontSize, 10);
-    el.style.fontSize = (curFont + step) + 'px';
+    var fontSize = parseInt(el.style.fontSize, 10);
+    if (fontSize >= 10 && fontSize <= 24) {
+        step = parseInt(step, 10);
+        var curFont = parseInt(el.style.fontSize, 10);
+        el.style.fontSize = (curFont + step) + 'px';
+    }
+    else if (fontSize <= 10 && step == 2) {
+        step = parseInt(step, 10);
+        var curFont = parseInt(el.style.fontSize, 10);
+        el.style.fontSize = (curFont + step) + 'px';
+    }
+    else if (fontSize >= 22 && step == -2) {
+        step = parseInt(step, 10);
+        var curFont = parseInt(el.style.fontSize, 10);
+        el.style.fontSize = (curFont + step) + 'px';
+    }
 }
