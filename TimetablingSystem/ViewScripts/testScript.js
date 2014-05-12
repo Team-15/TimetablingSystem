2afe4abf3ed6a42d4da2f04dda03abde580b0152
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2014-05-12 12:43:23 +0100

    Fixed Contrast JS

--- TimetablingSystem/ViewScripts/testScript.js
+++ TimetablingSystem/ViewScripts/testScript.js
@@ -40,10 +40,17 @@ function changeFontSize(element, step) {
     head = document.getElementsByTagName('head')[0],
     style = document.createElement('style');
 
+    style.setAttribute("id", "invertStyle");
+
     // a hack, so you can "invert back" clicking the bookmarklet again
     if (!window.counter) { window.counter = 1; } else {
         window.counter++;
-        if (window.counter % 2 == 0) { var css = 'html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }' }
+        if (window.counter % 2 == 0) {
+
+            var invStyle = document.getElementById("invertStyle");
+            head.removeChild(invStyle);
+
+        }
     };
 
     style.type = 'text/css';
