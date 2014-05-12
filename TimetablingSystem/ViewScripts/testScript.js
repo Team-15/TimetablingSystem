31f82094e5d3e73cb966f52594b3b897d271912b
nathan1993 <nathan_n1993@yahoo.co.uk>
2014-05-12 01:28:22 +0100

    TextSize and contrast Working

--- TimetablingSystem/ViewScripts/testScript.js
+++ TimetablingSystem/ViewScripts/testScript.js
@@ -5,7 +5,7 @@
 });
 function defaultFont(element) {
     var el = document.getElementById(element);
-    el.style.fontSize = 12 + 'px';
+    el.style.fontSize = 14 + 'px';
 
 }
 
@@ -27,4 +27,32 @@ function changeFontSize(element, step) {
         var curFont = parseInt(el.style.fontSize, 10);
         el.style.fontSize = (curFont + step) + 'px';
     }
+}function negativar() {
+    // the css we are going to inject
+    var css = 'html {-webkit-filter: invert(100%);' +
+        '-moz-filter: invert(100%);' +
+        '-o-filter: invert(100%);' +
+        '-ms-filter: invert(100%); ' +
+        'filter: invert(100%); ' +
+        'filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'invert\'><feColorMatrix in=\'SourceGraphic\' type=\'matrix\' values=\'-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0\'/></filter></svg>#invert"); }'
+    ,
+
+    head = document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+    // a hack, so you can "invert back" clicking the bookmarklet again
+    if (!window.counter) { window.counter = 1; } else {
+        window.counter++;
+        if (window.counter % 2 == 0) { var css = 'html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }' }
+    };
+
+    style.type = 'text/css';
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+    //injecting the css to the head
+    head.appendChild(style);
 }
